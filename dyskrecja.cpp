456c455bc4c496999109f6c4d59332f67d064e11
Namuennov <jakub.forys@opoczta.pl>
2023-11-22 23:52:08 +0100

    Switching from macros to booleans

--- dyskrecja.cpp
+++ dyskrecja.cpp
@@ -10,80 +10,85 @@
 
 int main(int argc, char** argv)
 {
+    bool PERFORM_CELLULAR_AUTOMATA_GRAIN_GROWTH = false;
+    bool PERFORM_MONTE_CARLO_GRAIN_GROWTH = false;
+
     //zaczynamy od 1, bo pierwszy argument to nazwa programu
     for (int i = 1; i < argc; i++) {
         
         Config config(static_cast<std::string>(argv[i]));
         if (config.CA_noNucleons > 0) {
-#define PERFORM_CELLULAR_AUTOMATA_GRAIN_GROWTH
+            PERFORM_CELLULAR_AUTOMATA_GRAIN_GROWTH = true;
         }
         if (config.MC_noSteps > 0) {
-#define PERFORM_MONTE_CARLO_GRAIN_GROWTH
+            PERFORM_MONTE_CARLO_GRAIN_GROWTH = true;
         }
 
         std::chrono::time_point<std::chrono::system_clock> startTime, endTime;
+        std::chrono::microseconds elapsedTimeInitCA, elapsedTimeSimulationCA, elapsedTimeWritingResultToFileCA;
+        std::chrono::microseconds elapsedTimeInitMC, elapsedTimeSimulationMC, elapsedTimeWritingResultToFileMC;
 
-#ifdef PERFORM_CELLULAR_AUTOMATA_GRAIN_GROWTH
-        startTime = std::chrono::system_clock::now();
-        Mesh* meshCA = new Mesh(config.meshSizeX, config.meshSizeY, config.meshSizeZ);
-        GrainGrowth* modelCA = new GrainGrowth(meshCA);
-        modelCA->setRandomInitialConditions(config.CA_noNucleons);
-        endTime = std::chrono::system_clock::now();
-        auto elapsedTimeInitCA = std::chrono::duration_cast<std::chrono::microseconds>(endTime - startTime);
+        if (PERFORM_CELLULAR_AUTOMATA_GRAIN_GROWTH) {
+            startTime = std::chrono::system_clock::now();
+            Mesh* meshCA = new Mesh(config.meshSizeX, config.meshSizeY, config.meshSizeZ);
+            GrainGrowth* modelCA = new GrainGrowth(meshCA);
+            modelCA->setRandomInitialConditions(config.CA_noNucleons);
+            endTime = std::chrono::system_clock::now();
+            elapsedTimeInitCA = std::chrono::duration_cast<std::chrono::microseconds>(endTime - startTime);
 
-        startTime = std::chrono::system_clock::now();
-        modelCA->runSimulationCA(config.boundaryConditionType, config.neighbourhoodType, config.simulationName);
-        endTime = std::chrono::system_clock::now();
-        auto elapsedTimeSimulationCA = std::chrono::duration_cast<std::chrono::microseconds>(endTime - startTime);
+            startTime = std::chrono::system_clock::now();
+            modelCA->runSimulationCA(config.boundaryConditionType, config.neighbourhoodType, config.simulationName);
+            endTime = std::chrono::system_clock::now();
+            elapsedTimeSimulationCA = std::chrono::duration_cast<std::chrono::microseconds>(endTime - startTime);
 
-        startTime = std::chrono::system_clock::now();
-        meshCA->saveStateToVTK("simulationResultCA_" + config.simulationName + ".vtk");
-        endTime = std::chrono::system_clock::now();
-        auto elapsedTimeWritingResultToFileCA = std::chrono::duration_cast<std::chrono::microseconds>(endTime - startTime);
+            startTime = std::chrono::system_clock::now();
+            meshCA->saveStateToVTK("simulationResultCA_" + config.simulationName + ".vtk");
+            endTime = std::chrono::system_clock::now();
+            elapsedTimeWritingResultToFileCA = std::chrono::duration_cast<std::chrono::microseconds>(endTime - startTime);
 
-        delete modelCA;
-        delete meshCA;
-#endif
+            delete modelCA;
+            delete meshCA;
+        }
 
 
-#ifdef PERFORM_MONTE_CARLO_GRAIN_GROWTH
-        startTime = std::chrono::system_clock::now();
-        Mesh* meshMC = new Mesh(config.meshSizeX, config.meshSizeY, config.meshSizeZ);
-        GrainGrowth* modelMC = new GrainGrowth(meshMC);
-        modelMC->setRandomNotZeroStateInEveryCellInMesh();
-        endTime = std::chrono::system_clock::now();
-        auto elapsedTimeInitMC = std::chrono::duration_cast<std::chrono::microseconds>(endTime - startTime);
+        if (PERFORM_MONTE_CARLO_GRAIN_GROWTH) {
+            startTime = std::chrono::system_clock::now();
+            Mesh* meshMC = new Mesh(config.meshSizeX, config.meshSizeY, config.meshSizeZ);
+            GrainGrowth* modelMC = new GrainGrowth(meshMC);
+            modelMC->setRandomNotZeroStateInEveryCellInMesh();
+            endTime = std::chrono::system_clock::now();
+            elapsedTimeInitMC = std::chrono::duration_cast<std::chrono::microseconds>(endTime - startTime);
 
-        startTime = std::chrono::system_clock::now();
-        modelMC->runSimulationMC(config.boundaryConditionType, config.neighbourhoodType, config.MC_noSteps, config.simulationName);
-        endTime = std::chrono::system_clock::now();
-        auto elapsedTimeSimulationMC = std::chrono::duration_cast<std::chrono::microseconds>(endTime - startTime);
+            startTime = std::chrono::system_clock::now();
+            modelMC->runSimulationMC(config.boundaryConditionType, config.neighbourhoodType, config.MC_noSteps, config.simulationName);
+            endTime = std::chrono::system_clock::now();
+            elapsedTimeSimulationMC = std::chrono::duration_cast<std::chrono::microseconds>(endTime - startTime);
 
-        startTime = std::chrono::system_clock::now();
-        meshMC->saveStateToVTK("simulationResultMC_" + config.simulationName + ".vtk");
-        endTime = std::chrono::system_clock::now();
-        auto elapsedTimeWritingResultToFileMC = std::chrono::duration_cast<std::chrono::microseconds>(endTime - startTime);
+            startTime = std::chrono::system_clock::now();
+            meshMC->saveStateToVTK("simulationResultMC_" + config.simulationName + ".vtk");
+            endTime = std::chrono::system_clock::now();
+            elapsedTimeWritingResultToFileMC = std::chrono::duration_cast<std::chrono::microseconds>(endTime - startTime);
 
-        delete modelMC;
-        delete meshMC;
-#endif
+            delete modelMC;
+            delete meshMC;
+        }
 
 
         std::string timeMeasurementsFilename = "timeMeasurements_" + config.simulationName + ".txt";
         std::ofstream timeMeasurementsFile(timeMeasurementsFilename);
-#ifdef PERFORM_CELLULAR_AUTOMATA_GRAIN_GROWTH
-        timeMeasurementsFile << "Time of initialization CA: " << elapsedTimeInitCA.count() << " microseconds\n";
-        timeMeasurementsFile << "Time of simulation CA: " << elapsedTimeSimulationCA.count() << " microseconds\n";
-        timeMeasurementsFile << "Time of writing the result to a file CA: " << elapsedTimeWritingResultToFileCA.count() << " microseconds";
-#endif
-#ifdef PERFORM_MONTE_CARLO_GRAIN_GROWTH
-        timeMeasurementsFile << "Time of initialization MC: " << elapsedTimeInitMC.count() << " microseconds\n";
-        timeMeasurementsFile << "Time of simulation MC: " << elapsedTimeSimulationMC.count() << " microseconds\n";
-        timeMeasurementsFile << "Time of writing the result to a file MC: " << elapsedTimeWritingResultToFileMC.count() << " microseconds";
-#endif
+        if (PERFORM_CELLULAR_AUTOMATA_GRAIN_GROWTH) {
+            timeMeasurementsFile << "Time of initialization CA: " << elapsedTimeInitCA.count() << " microseconds\n";
+            timeMeasurementsFile << "Time of simulation CA: " << elapsedTimeSimulationCA.count() << " microseconds\n";
+            timeMeasurementsFile << "Time of writing the result to a file CA: " << elapsedTimeWritingResultToFileCA.count() << " microseconds";
+        }
+        if (PERFORM_MONTE_CARLO_GRAIN_GROWTH) {
+            timeMeasurementsFile << "Time of initialization MC: " << elapsedTimeInitMC.count() << " microseconds\n";
+            timeMeasurementsFile << "Time of simulation MC: " << elapsedTimeSimulationMC.count() << " microseconds\n";
+            timeMeasurementsFile << "Time of writing the result to a file MC: " << elapsedTimeWritingResultToFileMC.count() << " microseconds";
+        }
         timeMeasurementsFile.close();
 
-#undef PERFORM_CELLULAR_AUTOMATA_GRAIN_GROWTH
-#undef PERFORM_MONTE_CARLO_GRAIN_GROWTH
+        PERFORM_CELLULAR_AUTOMATA_GRAIN_GROWTH = false;
+        PERFORM_MONTE_CARLO_GRAIN_GROWTH = false;
     }
 }
